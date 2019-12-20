268c2eefb5e7cf7aa6983fb9dc55e93bc376fc0f
Sami <sami79031@yahoo.com>
2019-12-20 11:51:15 +0200

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Sami Ali on 12/20/19.
+//  Copyright © 2019 Sami Ali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
